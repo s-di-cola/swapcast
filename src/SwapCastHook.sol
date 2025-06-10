237f46139ac64df362f451ff4bd6bbce2f2cc9d2
Felipe Faria <felipeasf@gmail.com>
2025-06-09 23:38:22 -0300

    fix solidity version to 0.8.26

--- src/SwapCastHook.sol
+++ src/SwapCastHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity 0.8.26;
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
