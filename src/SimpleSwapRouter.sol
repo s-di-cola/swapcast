2a504432eccf29aef2735bdc1afbc13ff82b0335
Felipe Faria <felipeasf@gmail.com>
2025-06-09 23:38:22 -0300

    fix solidity version to 0.8.26

--- src/SimpleSwapRouter.sol
+++ src/SimpleSwapRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity 0.8.26;
 
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
